24244535e5012dec10001c0e992e0d635ec91efa
SayRetrast <retrast.keke3h@gmail.com>
2024-09-21 01:27:45 +0700

    add factorial calc

--- SecondLab/app/src/main/java/com/example/secondlab/MainActivity.kt
+++ SecondLab/app/src/main/java/com/example/secondlab/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.example.secondlab
 
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.widget.addTextChangedListener
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +19,37 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val toast = Toast.makeText(this, "", Toast.LENGTH_SHORT)
+
+        val editText = findViewById<EditText>(R.id.editText)
+
+        editText.addTextChangedListener {
+            val editTextValue = editText.text.toString()
+
+            if (editTextValue == "") {
+                return@addTextChangedListener;
+            }
+
+            val numberValue = editTextValue.toIntOrNull()
+            if (numberValue != null) {
+                if (numberValue <= 0) {
+                    toastHandler(toast, "Это не натуральное число");
+                    return@addTextChangedListener;
+                }
+
+                var facrotial: Int = 1
+                for (i in 2..numberValue) {
+                    facrotial *= i;
+                }
+
+                return@addTextChangedListener;
+            }
+        }
+    }
+
+    private fun toastHandler(toast: Toast, text: String) {
+        toast.setText(text)
+        toast.show()
     }
 }
