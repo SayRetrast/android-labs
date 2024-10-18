8284a795d8f887ca722ea54d5a1db67c0c54c1f1
SayRetrast <retrast.keke3h@gmail.com>
2024-10-19 00:46:45 +0700

    create questions

--- FourthLab/app/src/main/java/com/example/fourthlab/MainActivity.kt
+++ FourthLab/app/src/main/java/com/example/fourthlab/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.fourthlab
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +17,32 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val questions = arrayOf(
+            mapOf(
+                "question" to "Является ли Земля четвертой планетой от Солнца?",
+                "answer" to false,
+            ),
+            mapOf(
+                "question" to "Существует ли жизнь на других планетах в нашей галактике?",
+                "answer" to false,
+            ),
+            mapOf(
+                "question" to "Является ли вода химическим соединением?",
+                "answer" to false,
+            ),
+            mapOf(
+                "question" to "Могут ли некоторые животные спать под водой?",
+                "answer" to false,
+            ),
+            mapOf(
+                "question" to "Является ли электричество формой энергии?",
+                "answer" to false,
+            ),
+        )
+        val currentQuestion = 0
+
+        val textView: TextView = findViewById(R.id.question)
+        textView.text = questions[currentQuestion]["question"].toString()
     }
 }
