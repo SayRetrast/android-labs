3227078d1b0d984e02ec81d646840ae91f5df2d6
SayRetrast <retrast.keke3h@gmail.com>
2024-10-19 01:36:56 +0700

    override onSaveInstanceState

--- FourthLab/app/src/main/java/com/example/fourthlab/MainActivity.kt
+++ FourthLab/app/src/main/java/com/example/fourthlab/MainActivity.kt
@@ -10,6 +10,37 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private  val questions = arrayOf(
+        mapOf(
+            "question" to "Является ли Земля четвертой планетой от Солнца?",
+            "answer" to false,
+        ),
+        mapOf(
+            "question" to "Существует ли жизнь на других планетах в нашей галактике?",
+            "answer" to false,
+        ),
+        mapOf(
+            "question" to "Является ли вода химическим соединением?",
+            "answer" to true,
+        ),
+        mapOf(
+            "question" to "Могут ли некоторые животные спать под водой?",
+            "answer" to true,
+        ),
+        mapOf(
+            "question" to "Является ли электричество формой энергии?",
+            "answer" to true,
+        ),
+    )
+    private var currentQuestion = 0
+    private var correctAnswers = 0
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt("currentQuestion", this.currentQuestion)
+        outState.putInt("correctAnswers", this.correctAnswers)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -20,41 +51,26 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
-        val questions = arrayOf(
-            mapOf(
-                "question" to "Является ли Земля четвертой планетой от Солнца?",
-                "answer" to false,
-            ),
-            mapOf(
-                "question" to "Существует ли жизнь на других планетах в нашей галактике?",
-                "answer" to false,
-            ),
-            mapOf(
-                "question" to "Является ли вода химическим соединением?",
-                "answer" to true,
-            ),
-            mapOf(
-                "question" to "Могут ли некоторые животные спать под водой?",
-                "answer" to true,
-            ),
-            mapOf(
-                "question" to "Является ли электричество формой энергии?",
-                "answer" to true,
-            ),
-        )
-        var currentQuestion = 0
-        var correctAnswers = 0
-
         val textView: TextView = findViewById(R.id.question)
-        textView.text = questions[currentQuestion]["question"].toString()
-
         val buttonFalse: Button = findViewById(R.id.button_false)
         val buttonTrue: Button = findViewById(R.id.button_true)
         val buttonNext: Button = findViewById(R.id.button_next)
 
+        if (savedInstanceState != null) {
+            currentQuestion = savedInstanceState.getInt("currentQuestion")
+            correctAnswers = savedInstanceState.getInt("correctAnswers")
+
+            if (currentQuestion == 5) {
+                textView.text = "Количество правильных ответов: ${this.correctAnswers}"
+                return
+            }
+        }
+
+        textView.text = this.questions[this.currentQuestion]["question"].toString()
+
         buttonFalse.setOnClickListener {
-            if (questions[currentQuestion]["answer"] == false) {
-                correctAnswers += 1
+            if (this.questions[this.currentQuestion]["answer"] == false) {
+                this.correctAnswers += 1
             }
 
             buttonFalse.visibility = View.INVISIBLE
@@ -62,8 +78,8 @@ class MainActivity : AppCompatActivity() {
         }
 
         buttonTrue.setOnClickListener {
-            if (questions[currentQuestion]["answer"] == true) {
-                correctAnswers += 1
+            if (this.questions[this.currentQuestion]["answer"] == true) {
+                this.correctAnswers += 1
             }
 
             buttonFalse.visibility = View.INVISIBLE
@@ -75,19 +91,19 @@ class MainActivity : AppCompatActivity() {
                 return@setOnClickListener
             }
 
-            currentQuestion += 1
+            this.currentQuestion += 1
 
-            if (currentQuestion == 5) {
+            if (this.currentQuestion == 5) {
                 buttonNext.isEnabled = false
                 buttonNext.isClickable = false
                 buttonNext.visibility = View.INVISIBLE
 
-                textView.text = "Количество правильных ответов: ${correctAnswers}"
+                textView.text = "Количество правильных ответов: ${this.correctAnswers}"
 
                 return@setOnClickListener
             }
 
-            textView.text = questions[currentQuestion]["question"].toString()
+            textView.text = this.questions[this.currentQuestion]["question"].toString()
             buttonFalse.visibility = View.VISIBLE
             buttonTrue.visibility = View.VISIBLE
         }
