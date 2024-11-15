b8088c1b586f13321b56a339a6d9f1d21d93c584
SayRetrast <retrast.keke3h@gmail.com>
2024-11-16 01:07:32 +0700

    add seekbar handler

--- LabFifth/app/src/main/java/com/example/labfifth/MainActivity.kt
+++ LabFifth/app/src/main/java/com/example/labfifth/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.example.labfifth
 
 import android.os.Bundle
+import android.widget.SeekBar
+import android.widget.SeekBar.OnSeekBarChangeListener
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var discountTextView: TextView
+    private lateinit var discountSeekBar: SeekBar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +22,22 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        discountTextView = findViewById(R.id.discount_text)
+        discountSeekBar = findViewById(R.id.discount_seekbar)
+
+        discountSeekBar.setOnSeekBarChangeListener(object : OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
+                val discountText = "Скидка: ${progress}%"
+                discountTextView.text = discountText
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+
+            }
+        })
     }
 }
