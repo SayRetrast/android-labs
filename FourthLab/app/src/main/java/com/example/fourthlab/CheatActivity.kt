c0c2be1115c7e92189dee2df4fc6393f2a1e4dbb
SayRetrast <retrast.keke3h@gmail.com>
2024-11-15 14:41:32 +0700

    add android sdk version

--- FourthLab/app/src/main/java/com/example/fourthlab/CheatActivity.kt
+++ FourthLab/app/src/main/java/com/example/fourthlab/CheatActivity.kt
@@ -1,6 +1,7 @@
 package com.example.fourthlab
 
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
@@ -11,6 +12,7 @@ class CheatActivity : AppCompatActivity() {
     private lateinit var answerTextView: TextView
     private lateinit var buttonShowAnswer: Button
     private lateinit var buttonBack: Button
+    private lateinit var androidApiTextView: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +21,10 @@ class CheatActivity : AppCompatActivity() {
         answerTextView = findViewById(R.id.cheat_answer)
         buttonShowAnswer = findViewById(R.id.show_answer)
         buttonBack = findViewById(R.id.close_cheat)
+        androidApiTextView = findViewById(R.id.android_api)
+
+        val androidApiText = "Android API: ${Build.VERSION.SDK_INT}"
+        androidApiTextView.text = androidApiText
 
         val isAnswerTrue = intent.getBooleanExtra("ANSWER", false)
         val answerText = if (isAnswerTrue) "Правильный ответ: Да" else "Правильный ответ: Нет"
