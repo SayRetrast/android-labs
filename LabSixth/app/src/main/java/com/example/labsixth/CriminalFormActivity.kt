4caba4064cefedc76376a6f8026f90f710ad3e83
SayRetrast <retrast.keke3h@gmail.com>
2024-11-20 15:02:34 +0700

    add starting layout

--- LabSixth/app/src/main/java/com/example/labsixth/CriminalFormActivity.kt
+++ LabSixth/app/src/main/java/com/example/labsixth/CriminalFormActivity.kt
@@ -1,6 +1,5 @@
 package com.example.labsixth
 
-import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
@@ -8,24 +7,23 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
-    private lateinit var criminalFormButton: Button
+class CriminalFormActivity : AppCompatActivity() {
+    private lateinit var backButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_criminal_form)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        criminalFormButton = findViewById(R.id.criminal_form_button)
+        backButton = findViewById(R.id.back_button)
 
-        criminalFormButton.setOnClickListener {
-            val criminalFormIntent = Intent(this, CriminalFormActivity::class.java)
-            startActivity(criminalFormIntent)
+        backButton.setOnClickListener {
+            finish()
         }
     }
 }
